a1a2c048f9f69977c2d09e8f7cbaad2270e1f736
MEJDIGHORRI <35100288+MEJDIGHORRI@users.noreply.github.com>
2020-03-05 10:58:25 +0100

    home mode zone added

--- src/main/webapp/app/home/home.component.ts
+++ src/main/webapp/app/home/home.component.ts
@@ -4,6 +4,7 @@ import { Subscription } from 'rxjs';
 import { LoginModalService } from 'app/core/login/login-modal.service';
 import { AccountService } from 'app/core/auth/account.service';
 import { Account } from 'app/core/user/account.model';
+import { faLightbulb, faTint, faThermometerHalf } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'jhi-home',
@@ -13,6 +14,26 @@ import { Account } from 'app/core/user/account.model';
 export class HomeComponent implements OnInit, OnDestroy {
   account: Account | null = null;
   authSubscription?: Subscription;
+  faLightbulb = faLightbulb;
+  faThermometerHalf = faThermometerHalf;
+  faTint = faTint;
+  tempIcone: any = '../../../content/icons/temp.png';
+  humIcone: any = '../../../content/icons/hum2.png';
+  bulbIcone: any = '../../../content/icons/bulb.png';
+  shutterIcone: any = '../../../content/icons/shutter.png';
+  storeIcone: any = '../../../content/icons/store.png';
+  heatingIcone: any = '../../../content/icons/heating.png';
+  camIcone: any = '../../../content/icons/cam.png';
+  gpsIcone: any = '../../../content/icons/gps.png';
+  livingroom: any = '../../../content/images/zone/living-room.jpg';
+  bedroom: any = '../../../content/images/zone/bedroom.jpg';
+  bedroom2: any = '../../../content/images/zone/bedroom2.jpg';
+  kitchen: any = '../../../content/images/zone/kitchen.jpg';
+  garden: any = '../../../content/images/zone/garden.jpg';
+  swimmingpool: any = '../../../content/images/zone/swimming-pool.jpg';
+  bathroom: any = '../../../content/images/zone/bathroom.jpg';
+  hall: any = '../../../content/images/zone/hall.jpg';
+  terrace: any = '../../../content/images/zone/terrace.jpg';
 
   constructor(private accountService: AccountService, private loginModalService: LoginModalService) {}
 
